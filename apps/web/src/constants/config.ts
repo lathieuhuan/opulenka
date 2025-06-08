00c093cb778c68a2676e07061bdd0fd57837c101
lathieuhuan <lathieuhuan52@gmail.com>
2025-06-08 18:53:40 +0700

    build: add dist to turbo build output; correct checking is dev env

--- apps/web/src/constants/config.ts
+++ apps/web/src/constants/config.ts
@@ -1,4 +1,4 @@
-export const IS_DEV_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV !== "production";
+export const IS_DEV_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
 
 const protocol = IS_DEV_ENV ? "http" : "https";
 const domain = process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000";
