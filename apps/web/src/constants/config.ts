981e1c7c4d326cce3f66a96e16df7e0a777b48d9
lathieuhuan <lathieuhuan52@gmail.com>
2025-06-08 19:05:47 +0700

    build(web): use production url for domain in production

--- apps/web/src/constants/config.ts
+++ apps/web/src/constants/config.ts
@@ -1,5 +1,11 @@
-export const IS_DEV_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
+const ENV = process.env.NEXT_PUBLIC_VERCEL_ENV; // "development", "preview", "production"
+export const IS_DEV_ENV = ENV === "development";
 
 const protocol = IS_DEV_ENV ? "http" : "https";
-const domain = process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000";
+const domain =
+  ENV === "production"
+    ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+    : process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000";
+// NEXT_PUBLIC_VERCEL_URL is the generated deployment URL which contains the deployment ID.
+// Docs: https://vercel.com/docs/environment-variables/framework-environment-variables
 export const DOMAIN = `${protocol}://${domain}`;
