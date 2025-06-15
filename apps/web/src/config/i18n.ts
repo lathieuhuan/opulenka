f9d8e69662dae021a26ad68b67d9720c848e9823
lathieuhuan <lathieuhuan52@gmail.com>
2025-06-15 13:07:00 +0700

    feat(web): make dummy landing-page

--- apps/web/src/config/i18n.ts
+++ apps/web/src/config/i18n.ts
@@ -1,5 +1,9 @@
 import { getRequestConfig } from "next-intl/server";
 
+export const LOCALES = ["en"] as const;
+
+export type Locale = (typeof LOCALES)[number];
+
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
