7c38cd513d806e25914f722445e9f49959802e7f
lathieuhuan <lathieuhuan52@gmail.com>
2025-07-19 16:32:01 +0700

    remove unused type

--- apps/web/src/types/global.ts
+++ apps/web/src/types/global.ts
@@ -1,12 +1,5 @@
-import type { LucideProps } from "lucide-react";
-import type { ForwardRefExoticComponent, RefAttributes } from "react";
-
 export type UserInfo = {
   userId: number;
   email: string;
   username: string | null;
 };
-
-export type IconType = ForwardRefExoticComponent<
-  Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
->;
