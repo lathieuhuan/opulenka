f931de888523b417ef47182d47cd3fed8c88bb18
lathieuhuan <lathieuhuan52@gmail.com>
2026-08-11 01:31:25 +0700

    build(ui): re-structure styles in dev & prod
    - Split @fontsource-variable/noto-sans from index.css into src/fonts and build it separately
    - Remove index.build.css because we do not build css with tailwind anyway
    - Build index.css separately from src/index.ts
    - Re-order imports in the consuming stylesheets (web, storybook) because the layer base of tailwind wins over that of ui index.css
    - Also, upgrade vite-plugin-dts to 5.0.3 because of a bug in the current version. See https://github.com/qmhc/unplugin-dts/issues/330

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,61 +1,85 @@
 import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
+import { glob } from "glob"
 import { defineConfig, normalizePath } from "vite"
 import dts from "vite-plugin-dts"
 import { viteStaticCopy } from "vite-plugin-static-copy"
 
-export default defineConfig({
-	plugins: [
-		react(),
-		tailwindcss(),
-		dts({
-			tsconfigPath: "./tsconfig.build.json",
-		}),
-		viteStaticCopy({
-			targets: [
-				{
-					src: "src/themes/*.css",
-					dest: "themes",
-					rename: { stripBase: true },
-				},
-			],
-		}),
-	],
-	server: {
-		port: 3001,
-	},
-	esbuild: {
-		target: "es2022",
-	},
-	resolve: {
-		alias: {
-			"@": normalizePath(path.resolve(__dirname, "./src")),
+function pathTo(p: string) {
+	return normalizePath(path.resolve(__dirname, p))
+}
+
+export default defineConfig(() => {
+	const fontEntries = Object.fromEntries(
+		glob
+			.sync("src/fonts/*.css")
+			.map((file) => [path.basename(file, ".css"), pathTo(file)]),
+	)
+
+	return {
+		root: __dirname,
+		plugins: [
+			react(),
+			tailwindcss(),
+			dts({
+				tsconfigPath: "./tsconfig.build.json",
+			}),
+			viteStaticCopy({
+				targets: [
+					{
+						src: "src/themes/*.css",
+						dest: "themes",
+						rename: { stripBase: true },
+					},
+				],
+			}),
+		],
+		server: {
+			port: 3001,
 		},
-	},
-	build: {
-		outDir: "dist",
-		lib: {
-			entry: {
-				index: normalizePath(path.resolve(__dirname, "src/index.ts")),
-			},
-			formats: ["es"],
+		esbuild: {
+			target: "es2022",
 		},
-		cssCodeSplit: true,
-		copyPublicDir: false,
-		rollupOptions: {
-			external: ["react", "react/jsx-runtime"],
-			output: {
-				format: "es",
-				entryFileNames: "[name].js",
-				assetFileNames: (assetInfo) => {
-					if (assetInfo.originalFileNames.includes("src/index.ts")) {
-						return "index.css"
-					}
-					// Default naming for other assets
-					return "assets/[name].[ext]"
-				},
+		resolve: {
+			alias: {
+				"@": pathTo("src"),
 			},
 		},
-	},
+		build: {
+			outDir: "dist",
+			lib: {
+				entry: {
+					index: pathTo("src/index.ts"),
+					"index-css": pathTo("src/styles/index.css"),
+					...fontEntries,
+				},
+				formats: ["es"],
+			},
+			cssCodeSplit: true,
+			copyPublicDir: false,
+			rollupOptions: {
+				external: ["react", "react/jsx-runtime"],
+				output: {
+					format: "es",
+					entryFileNames: "[name].js",
+					assetFileNames: (assetInfo) => {
+						const originalName = assetInfo.originalFileNames.at(0) || ""
+
+						if (originalName.startsWith("src/fonts")) {
+							return "fonts/[name][extname]"
+						}
+
+						if (originalName.startsWith("src/styles")) {
+							const name = originalName.replace("src/styles/", "")
+							return `styles/${name}`
+						}
+
+						// Default naming for other assets
+						return "assets/[name].[ext]"
+					},
+				},
+			},
+		},
+	}
 })
